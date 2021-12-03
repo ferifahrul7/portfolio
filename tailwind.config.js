028d3d21541135b02bff80f8a14a7d3c0bf1eb12
ferifahrul7 <ferifahrul7@gmail.com>
2021-12-03 10:54:31 +0700

    fix purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 module.exports = {
     purge: [
-        './src/**/*.html',
-        './src/**/*.js',
+        './*.html',
+        // './src/**/*.js',
     ],
     darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
-    },
     variants: {
         extend: {},
     },
