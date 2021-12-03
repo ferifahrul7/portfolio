118e871c12a2e1345e1d16d66d3da2e4a871bf4c
ferifahrul7 <ferifahrul7@gmail.com>
2021-12-03 09:52:40 +0700

    add npm command

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('postcss-100vh-fix'),
+        require('autoprefixer'),
+    ]
+}
